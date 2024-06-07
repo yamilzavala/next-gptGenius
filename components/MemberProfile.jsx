50e4f76d01a56201e5c20ad7cdf0e75ba2e1e2e5
yamilzavala <zavalayamil9@gmail.com>
2024-06-07 05:34:39 -0300

    removed tokens logic

--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -1,15 +1,19 @@
 import { UserButton } from '@clerk/nextjs';
 import { currentUser, auth  } from '@clerk/nextjs/server';
-import { fetchOrGenerateTokens } from '@/utils/actions';
+//import { fetchOrGenerateTokens } from '@/utils/actions';
 
 const MemberProfile = async () => {
     const user = await currentUser();
     const { userId } = auth();
-    //await fetchOrGenerateTokens(userId);
+   // const currentTokens = await fetchUserTokensById(userId);
+    if(!user) return null;
     return (
       <div className='px-4 flex items-center gap-2'>
-        <UserButton afterSignOutUrl='/' />
-        <p>{user?.emailAddresses[0]?.emailAddress}</p>
+        {/* <h2 className='mb-8 ml-8 text-xl font-extrabold'>
+          Token Amount : {currentTokens}
+        </h2> */}
+        <UserButton routing='hash' />
+        <p>Manage account</p>
       </div>
     );
   };
--- utils/actions.js
+++ utils/actions.js
@@ -1,6 +1,7 @@
 'use server'
 import OpenAI from "openai";
 import prisma from "@/utils/db";
+import { revalidatePath } from "next/cache";
 
 const openai = new OpenAI({
     apiKey:process.env.OPENAI_API_KEY,
@@ -14,7 +15,8 @@ export const generateChatResponse = async (chatMessages) => {
                 ...chatMessages
             ],
             model: 'gpt-3.5-turbo',
-            temperature: 0
+            temperature: 0,
+            max_tokens: 100
         })    
         console.log(response.choices[0].message);
         console.log(response);
@@ -133,3 +135,46 @@ export const generateTourImage = async ({city, country}) => {
         return null;
     }
 }
+
+// export const fetchUserTokensById = async (clerkId) => {
+//     const result = await prisma.token.findUnique({
+//       where: {
+//         clerkId,
+//       },
+//     });
+  
+//     return result?.tokens;
+//   };
+  
+//   export const generateUserTokensForId = async (clerkId) => {
+//     const result = await prisma.token.create({
+//       data: {
+//         clerkId,
+//       },
+//     });
+//     return result?.tokens;
+//   };
+  
+//   export const fetchOrGenerateTokens = async (clerkId) => {
+//     const result = await fetchUserTokensById(clerkId);
+//     if (result) {
+//       return result.tokens;
+//     }
+//     return (await generateUserTokensForId(clerkId)).tokens;
+//   };
+  
+//   export const subtractTokens = async (clerkId, tokens) => {
+//     const result = await prisma.token.update({
+//       where: {
+//         clerkId,
+//       },
+//       data: {
+//         tokens: {
+//           decrement: tokens,
+//         },
+//       },
+//     });
+//     revalidatePath('/profile');
+//     // Return the new token value
+//     return result.tokens;
+//   };
